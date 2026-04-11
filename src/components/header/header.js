2b961fe69eb669bf9707e91ea5378e12eb4510db
your GitHub login <zenia.codes@gmail.com>
2026-04-11 19:54:14 +0200

    style: adjust hamburger and JS in menu navigation

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,12 +1,30 @@
-const checkbox = document.querySelector(".menu_button_checkbox");
 const menuLinks = document.querySelectorAll(".navigation_menu a");
+const menu = document.querySelector(".navigation_menu");
+const button = document.querySelector(".menu_button");
 
-// přidání posluchače na všechny odkazy v menu
+function closeMenu() {
+  menu.classList.remove("is-open"); // zavře menu - odstraní třídu .is-open
+  button.setAttribute("aria-expanded", "false"); // nastaví button jako „zavřený stav“
+  button.setAttribute("aria-label", "Open menu"); // změna popisku buttonu (řekne uživateli, že ho může otevřít)
+}
+
+function openMenu() {
+  menu.classList.add("is-open");
+  button.setAttribute("aria-expanded", "true");
+  button.setAttribute("aria-label", "Close menu");
+}
+
 menuLinks.forEach((link) => {
-  link.addEventListener("click", () => {
-    checkbox.checked = false;
-  });
+  link.addEventListener("click", closeMenu);
 });
 
-console.log(menuLinks);
-console.log(checkbox);
+button.addEventListener("click", () => {
+  const isOpen = button.getAttribute("aria-expanded") === "true";
+  isOpen ? closeMenu() : openMenu(); // Ternární operátor místo if a else (podmínka ? co_když_platí : co_když_neplatí;)
+});
+// zavření menu odkudkoli na stránce
+document.addEventListener("click", (e) => {
+  if (!menu.contains(e.target) && !button.contains(e.target)) {
+    closeMenu();
+  }
+});
