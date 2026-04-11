f9553a522b4bb638ee86e6e4fbb57551e88b7754
your GitHub login <zenia.codes@gmail.com>
2026-04-11 20:05:14 +0200

    Update content

--- docs/my_web_SenseSpace.795408dd.js
+++
@@ -1,2 +0,0 @@
-let e=document.querySelector(".menu_button_checkbox"),o=document.querySelectorAll(".navigation_menu a");o.forEach(o=>{o.addEventListener("click",()=>{e.checked=!1})}),console.log(o),console.log(e);
-//# sourceMappingURL=my_web_SenseSpace.795408dd.js.map
