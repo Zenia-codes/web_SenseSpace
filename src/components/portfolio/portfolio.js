be0b489bf6fd9f2d74d475ca6849d2ed86c44160
your GitHub login <zenia.codes@gmail.com>
2026-05-09 22:37:21 +0200

    feat: section Portfolio - add slider

--- src/components/portfolio/portfolio.js
+++ src/components/portfolio/portfolio.js
@@ -1,1 +1,27 @@
+const dots = document.querySelectorAll(".dot");
+const slides = document.querySelectorAll(".project_card");
 
+let currentSlide = 0;
+
+function showSlide(index) {
+  // deaktivovat všechny tečky
+  dots.forEach((dot) => {
+    dot.classList.remove("active");
+  });
+
+  // zobrazit správný slide
+  slides[index].classList.add("active-slide");
+
+  // aktivovat správnou tečku
+  dots[index].classList.add("active");
+}
+
+dots.addEventListener("click", () => {
+  currentSlide++;
+
+  if (currentSlide >= slides.length) {
+    currentSlide = 0;
+  }
+
+  showSlide(currentSlide);
+});
