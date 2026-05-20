930862187b50bbec84f4f6dc6a4b4558b7c82f89
your GitHub login <zenia.codes@gmail.com>
2026-05-20 17:55:55 +0200

    refactor: add main.js and folder utils, clean up js scripts from html

---
+++ src/main.js
@@ -0,0 +1,5 @@
+import "./utils/reveal.js";
+import "./components/header/header.js";
+import "./components/hero/heroButton.js";
+import "./components/portfolio/portfolio.js";
+import "./components/portfolio/modal.js";
