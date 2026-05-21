050b9105fb72f7b2599df176392140123aa90ea1
your GitHub login <zenia.codes@gmail.com>
2026-05-21 08:45:52 +0200

    Update content

--- docs/my_web_SenseSpace.aa78fe5e.js
+++
@@ -1,2 +0,0 @@
-let e=document.querySelectorAll(".reveal"),r=new IntersectionObserver(e=>{e.forEach(e=>{e.isIntersecting&&e.target.classList.add("active")})},{threshold:.2});e.forEach(e=>r.observe(e));
-//# sourceMappingURL=my_web_SenseSpace.aa78fe5e.js.map
