619b8beba37df814a685e69341b6ee93e592bc4a
your GitHub login <zenia.codes@gmail.com>
2026-09-14 23:31:49 +0200

    refactor: add openModal and closeModal functions

--- src/components/portfolio/modal.js
+++ src/components/portfolio/modal.js
@@ -2,32 +2,30 @@ const modal = document.querySelector(".modal");
 const modalContent = document.querySelector(".modal__content");
 const closeBtn = document.querySelector(".modal__close");
 
+const openModal = function (modal) {
+  modal.classList.remove("hidden");
+  document.body.classList.add("modal__open");
+};
+
+const closeModal = function (modal) {
+  modal.classList.add("hidden");
+  document.body.classList.remove("modal__open");
+};
+
 document.querySelectorAll(".project_card").forEach((card) => {
   card.addEventListener("click", () => {
-    // vezme hodnotu z data-modal=""
     const modalId = card.dataset.modal;
-
-    // najde modal podle ID
     const modal = document.getElementById(modalId);
 
-    // zobrazí modal
-    modal.classList.remove("hidden");
-
-    // zakáže scroll stránky
-    document.body.classList.add("modal__open");
+    openModal(modal);
   });
 });
 
 document.querySelectorAll(".modal__close").forEach((button) => {
   button.addEventListener("click", () => {
-    // najde modal, ve kterém je tlačítko
     const modal = button.closest(".modal");
 
-    // schová modal
-    modal.classList.add("hidden");
-
-    // vrátí scroll stránky
-    document.body.classList.remove("modal__open");
+    closeModal(modal);
   });
 });
 
@@ -36,8 +34,7 @@ document.querySelectorAll(".modal").forEach((modal) => {
     const modalContent = modal.querySelector(".modal__content");
 
     if (!modalContent.contains(e.target)) {
-      modal.classList.add("hidden");
-      document.body.classList.remove("modal__open");
+      closeModal(modal);
     }
   });
 });
