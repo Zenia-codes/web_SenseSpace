362dd278a79f748cf16d91fd03b7563a286ae9bd
your GitHub login <zenia.codes@gmail.com>
2026-09-17 12:05:58 +0200

    fix: align split_row image correctly in tablet layout; refactor main.scss

--- src/main.js
+++ src/main.js
@@ -1,8 +1,9 @@
 import "./utils/reveal.js";
+import "./Buttons.js";
+import "./language.js";
+
 import "./components/header/header.js";
-import "./components/hero/heroButton.js";
 import "./components/portfolio/portfolio.js";
 import "./components/portfolio/modal.js";
-import "./language.js";
 import "./components/form/about_form.js";
 import "./components/form/form_validation.js";
